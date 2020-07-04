5048f7badfc13d1d442306d1bd5ab536778b3cb0
Oscar Alfredo  Cruz Rovira <cruzrovira@gmail.com>
2020-07-03 21:05:48 -0600

    Updates

---
+++ precache-manifest.9987562bf2f51153fc676e5afb6663f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "984426268c0cbd93d91ad9002dc8d98c",
+    "url": "/cruzrovira/expense-control/index.html"
+  },
+  {
+    "revision": "1970f22675abbe98511a",
+    "url": "/cruzrovira/expense-control/static/css/main.bdda1077.chunk.css"
+  },
+  {
+    "revision": "4bfbda8792db9dd7876e",
+    "url": "/cruzrovira/expense-control/static/js/2.b44a77dd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cruzrovira/expense-control/static/js/2.b44a77dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1970f22675abbe98511a",
+    "url": "/cruzrovira/expense-control/static/js/main.d04c7d2a.chunk.js"
+  },
+  {
+    "revision": "6ad8011f8bd1ec6673e4",
+    "url": "/cruzrovira/expense-control/static/js/runtime-main.8b66950d.js"
+  }
+]);
